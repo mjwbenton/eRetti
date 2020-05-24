5f4c7bb2b9ff091d1d47e3e09af5067e52290e13
Matthew Benton <m@mattb.tech>
2020-05-24 13:44:51 +0100

    Add ability to override file path locally

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@
 import Server from "./server";
 import Editor from "./editor";
 
-const STORAGE_FILE = "/mnt/kindlewriter/writing.txt";
+const STORAGE_FILE =
+  process.env.OVERRIDE_FILE || "/mnt/kindlewriter/writing.txt";
 
 async function main() {
   const editor = await Editor.createForPath(STORAGE_FILE);
